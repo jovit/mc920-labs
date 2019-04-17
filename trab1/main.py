5564bbcd742b48f490bb6728df8ae4d806d82a0f
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-04-17 14:40:39 -0300

    trabalho 1 finished

--- trab1/main.py
+++ trab1/main.py
@@ -11,7 +11,6 @@ import cv2
 from skimage import io, data_dir
 from skimage import data
 from skimage import img_as_float
-from scipy.ndimage.filters import convolve
 from matplotlib import pyplot as plt
 import math
 import numpy as np
@@ -85,3 +84,40 @@ composed = np.sqrt(np.power(image_h3.astype(float), 2) + np.power(image_h4.astyp
 cv2.normalize(composed, composed, 0, 255, cv2.NORM_MINMAX)
 io.imshow(composed.astype(int), cmap='gray')
 plt.show()
+
+#%%
+# Take the 2-dimensional DFT and centre the frequencies
+ftimage = np.fft.fft2(image)
+ftimage = np.fft.fftshift(ftimage)
+magnitude_spectrum = 20*np.log(np.abs(ftimage))
+io.imshow(np.abs(magnitude_spectrum), cmap='gray')
+plt.show()
+
+
+# Build and apply a Gaussian filter.
+frequencies = [10, 20, 30, 40, 50, 60]
+for d0 in frequencies:
+    nrows = len(image)
+    ncols = len(image[0])
+    cy, cx = nrows/2, ncols/2
+    # Generate the values for u, v
+    x = np.arange(nrows)
+    y = np.arange(ncols)
+    # create a 2D array for the value of u and v for each point
+    X, Y = np.meshgrid(x, y)
+    # make an gaussian mask, we subtract X and Y by c, because our fourrier is shifted
+    # so the origin is no longer in (0,0), so we need to shift the mask to the center
+    gmask = np.exp(
+        (-((X-cx) ** 2) / (2 * (d0 ** 2))) +
+        (-((Y-cy) ** 2) / (2 * (d0 ** 2) ))
+    )
+    
+    ftimagep = ftimage * gmask # apply tha mask
+    magnitude_spectrum = 20 * np.log(np.abs(ftimagep)) # generate a plottable spectrum
+    io.imshow(np.abs(magnitude_spectrum), cmap='gray')
+    plt.show()
+
+    # Finally, take the inverse transform and show the blurred image
+    imagep = np.fft.ifft2(ftimagep)
+    io.imshow(np.abs(imagep), cmap='gray')
+    plt.show()
